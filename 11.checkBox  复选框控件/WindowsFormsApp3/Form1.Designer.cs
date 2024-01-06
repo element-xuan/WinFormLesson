0479df3fd9913036c07323ae323ad06bd085e1fc
element <2279938087@qq.com>
2024-01-07 05:23:32 +0800

    Add CheckBox Control
    2024年1月7日05:23:19 上传复选框控件代码

---
+++ 11.checkBox  复选框控件/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(79, 92);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(134, 19);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "第一轮疫苗注射";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(79, 133);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(134, 19);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "第二轮疫苗注射";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Indeterminate;
+            this.checkBox3.Location = new System.Drawing.Point(79, 167);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(134, 19);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "第三轮疫苗注射";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            this.checkBox3.CheckStateChanged += new System.EventHandler(this.checkBox3_CheckStateChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "疫苗注射情况统计";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 222);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 11.checkBox  复选框控件/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //标志位  卡流程
+        //创建了三个标志位，当这三个状态都为true时，改变三个复选框控件的图标
+        bool CheckBox1Sate = false;
+        bool CheckBox2Sate = false;
+        bool CheckBox3Sate = false;
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.checkBox1.Checked == true)
+            {
+                CheckBox1Sate = true;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.checkBox2.Checked == true)
+            {
+                CheckBox2Sate = true;
+                CheckSate();
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.checkBox3.Checked == true)
+            {
+                CheckBox3Sate = true;
+            }
+
+            if (this.checkBox1.Checked == true && this.checkBox2.Checked == true && this.checkBox3.Checked == true)
+            {
+                MessageBox.Show("您的疫苗已经接种完毕！");
+                this.checkBox1.CheckState = CheckState.Indeterminate;
+                this.checkBox2.CheckState = CheckState.Indeterminate;
+                this.checkBox3.CheckState = CheckState.Indeterminate;
+
+                //将标志位 恢复为初始状态
+                CheckBox1Sate = false;
+                CheckBox2Sate = false;
+                CheckBox3Sate = false;
+            }
+            else
+            {
+                MessageBox.Show("您的疫苗还未接种完毕！");
+            }
+        }
+
+
+        void CheckSate(  )
+        {
+            if (this.checkBox1.Checked == true && this.checkBox2.Checked == true && this.checkBox3.Checked == true)
+            {
+                MessageBox.Show("您的疫苗已经接种完毕！");
+                this.checkBox1.CheckState = CheckState.Indeterminate;
+                this.checkBox2.CheckState = CheckState.Indeterminate;
+                this.checkBox3.CheckState = CheckState.Indeterminate;
+
+                //将标志位 恢复为初始状态
+                CheckBox1Sate = false;
+                CheckBox2Sate = false;
+                CheckBox3Sate = false;
+            }
+            else
+            {
+                MessageBox.Show("您的疫苗还未接种完毕！");
+            }
+
+        }
+
+        private void checkBox3_CheckStateChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.CheckState == CheckState.Checked)
+            {
+                MessageBox.Show("checkBox3 为点击状态");
+            }
+            else if (checkBox3.CheckState == CheckState.Unchecked)
+            {
+                MessageBox.Show("checkBox3 为未点击状态");
+            }
+            else if (checkBox3.CheckState == CheckState.Indeterminate) 
+            {
+                MessageBox.Show("checkBox3 为不确定状态");
+
+            }
+        }
+    }
+}
