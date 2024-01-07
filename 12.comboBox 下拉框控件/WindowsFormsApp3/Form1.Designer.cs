7ef2523c978a2f333128276217424b1e0a927024
element <2279938087@qq.com>
2024-01-08 03:13:23 +0800

    Add ComboBox Control
    2024年1月8日02:05:58  上传 下拉框控件代码

---
+++ 12.comboBox 下拉框控件/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnAddUser = new System.Windows.Forms.Button();
+            this.TBUserName = new System.Windows.Forms.TextBox();
+            this.TBUserID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox1.Location = new System.Drawing.Point(70, 1);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(411, 23);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "用户：";
+            // 
+            // BtnAddUser
+            // 
+            this.BtnAddUser.Location = new System.Drawing.Point(713, 106);
+            this.BtnAddUser.Name = "BtnAddUser";
+            this.BtnAddUser.Size = new System.Drawing.Size(75, 23);
+            this.BtnAddUser.TabIndex = 2;
+            this.BtnAddUser.Text = "添加";
+            this.BtnAddUser.UseVisualStyleBackColor = true;
+            this.BtnAddUser.Click += new System.EventHandler(this.BtnAddUser_Click);
+            // 
+            // TBUserName
+            // 
+            this.TBUserName.Location = new System.Drawing.Point(643, 31);
+            this.TBUserName.Name = "TBUserName";
+            this.TBUserName.Size = new System.Drawing.Size(145, 25);
+            this.TBUserName.TabIndex = 3;
+            // 
+            // TBUserID
+            // 
+            this.TBUserID.Location = new System.Drawing.Point(643, 75);
+            this.TBUserID.Name = "TBUserID";
+            this.TBUserID.Size = new System.Drawing.Size(145, 25);
+            this.TBUserID.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(576, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "用户名：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(576, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "用户ID：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 250);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TBUserID);
+            this.Controls.Add(this.TBUserName);
+            this.Controls.Add(this.BtnAddUser);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnAddUser;
+        private System.Windows.Forms.TextBox TBUserName;
+        private System.Windows.Forms.TextBox TBUserID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ 12.comboBox 下拉框控件/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 学生对象的泛型集合
+        /// </summary>
+        List<User> listUserIinfo = new List<User>();
+
+        private void BtnAddUser_Click(object sender, EventArgs e)
+        {
+            //.....省略非空验证
+            #region 封装对象
+            User user = new User();
+            user.UserName = this.TBUserName.Text;
+            user.UserID = Convert.ToInt32(this.TBUserID.Text);
+            #endregion
+            listUserIinfo.Add(user);
+
+            MessageBox.Show("添加成功！");
+
+            //清空文本框控件内 text的值
+            this.TBUserName.Text = "";
+            this.TBUserID.Text = "";
+
+
+            AddIten();
+        }
+
+        void AddIten()
+        {
+            this.comboBox1.DataSource = null;
+
+            //数据绑定
+            this.comboBox1.DataSource = listUserIinfo;
+            this.comboBox1.DisplayMember = "UserName";
+
+
+            //this.comboBox1.ValueMember = "UserID";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.comboBox1.SelectedIndex = 0;
+        }
+    }
+}
